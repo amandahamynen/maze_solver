f1374c2a5c82f1a2390813900f0db2e415ac1f15
Amanda <amanda.hamynen@gmail.com>
2022-03-23 14:23:17 +0200

    Added prebuilt 5x5 maze

--- src/index.py
+++ src/index.py
@@ -2,8 +2,8 @@ from pyamaze import maze, agent
 
 # Tällä hetkellä vain testataan, että pyamaze toimii
 
-m = maze()
-m.CreateMaze()
+m = maze(5,5)
+m.CreateMaze(loadMaze="data/maze-55.csv")
 a=agent(m,filled=True,footprints=True)
 m.tracePath({a:m.path})
 m.run()
