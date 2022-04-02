5011ad005b83d1b69cd52a0152ceaaeabf33b25b
Amanda <amanda.hamynen@gmail.com>
2022-04-02 13:34:59 +0300

    Added docstrings

--- src/left_wall_follower.py
+++ src/left_wall_follower.py
@@ -1,6 +1,16 @@
 class LeftWallFollower:
 
+    """ Luokka, joka vastaa left wall follower algoritmin toteutuksesta. """
+
     def __init__(self, labyrintti, n):
+
+        """ Luokan konstruktori.
+        Args:
+            labyrintti: moduulin Pyamaze avulla luotu olio, jolla on käytössä
+            moduulin omat metodit.
+            n: neliömuotoisen labyrintin leveys/korkeus.
+        """
+
         self.labyrintti = labyrintti
         self.aloitus = [n,n]
         self.maali = [1,1]
@@ -10,6 +20,16 @@ class LeftWallFollower:
         self.etsinta = []
 
     def vasen_seina(self, ruutu):
+
+        """ Palauttaa tiedon siitä, onko vasemmalla puolella seinää.
+
+        Args:
+            ruutu: paikka labyrintissa, lista-tyyppinen muuttuja, jonka ensimmäinen
+            arvo on x-koordinaatti ja toinen y-koordinaatti.
+        Returns:
+            boolean-arvon, joka kertoo onko vasemmalla seinää.
+        """
+
         x = ruutu[0]
         y = ruutu[1]
         if self.katseen_suunta == 'N':
@@ -22,6 +42,16 @@ class LeftWallFollower:
             return self.labyrintti.maze_map[x,y]['E'] == 0
 
     def seina_edessa(self, ruutu):
+
+        """ Palauttaa tiedon siitä, onko edessä seinää.
+
+        Args:
+            ruutu: paikka labyrintissa, lista-tyyppinen muuttuja, jonka ensimmäinen
+            arvo on x-koordinaatti ja toinen y-koordinaatti.
+        Returns:
+            boolean-arvon, joka kertoo onko edessä seinää.
+        """
+
         x = ruutu[0]
         y = ruutu[1]
         if self.katseen_suunta == 'N':
@@ -34,6 +64,9 @@ class LeftWallFollower:
             return self.labyrintti.maze_map[x,y]['S'] == 0
 
     def kaanny_vasemmalle(self):
+
+        """ Kääntää katsetta 90 astetta vasemmalle. """
+
         if self.katseen_suunta == 'N':
             self.katseen_suunta = 'W'
         elif self.katseen_suunta == 'E':
@@ -44,6 +77,9 @@ class LeftWallFollower:
             self.katseen_suunta = 'E'
 
     def kaanny_oikealle(self):
+
+        """ Kääntää katsetta 90 astetta oikealle. """
+
         if self.katseen_suunta == 'N':
             self.katseen_suunta = 'E'
         elif self.katseen_suunta == 'E':
@@ -54,6 +90,9 @@ class LeftWallFollower:
             self.katseen_suunta = 'W'
 
     def mene_eteenpain(self):
+
+        """ Menee yhden ruudun verran katseen suuntaan. """
+
         x = self.nykyinen_ruutu[0]
         y = self.nykyinen_ruutu[1]
         if self.katseen_suunta == 'N':
@@ -66,6 +105,13 @@ class LeftWallFollower:
             self.nykyinen_ruutu = [x+1, y]
 
     def wall_follower(self):
+
+        """ Löytää reitin labyrintista noudattaen left wall follower algoritmia.
+
+        Returns:
+            etsinta: tyypiltään lista. Sisältää järjestyksessä ne ruudut, jotka
+            algoritmi käy läpi etsiessään maaliruutua labyrintissa.
+        """
 
         while not self.maali_loytynyt:
 
