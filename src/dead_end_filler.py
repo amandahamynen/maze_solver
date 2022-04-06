ab00a8c3b6363be051b1744beffd5bd928a0181a
Amanda <amanda.hamynen@gmail.com>
2022-04-06 19:36:43 +0300

    Made first version of dead end filling algorithm

---
+++ src/dead_end_filler.py
@@ -0,0 +1,163 @@
+class DeadEndFiller:
+
+    """ Luokka, joka vastaa dead-end filling algoritmin toiminnasta. """
+
+    def __init__(self, labyrintti, n):
+        self.labyrintti = labyrintti
+        self.aloitus = (n,n)
+        self.maali = (1,1)
+        self.n = n
+        self.nykyinen_ruutu = (self.aloitus[0], self.aloitus[1])
+        self.jaljet = self.luo_ruudukko(self.n)
+        self.etsinta = []
+        self.reitti = []
+        self.umpikujat = []
+        self.maali_loytynyt = False
+
+    def luo_ruudukko(self, n):
+
+        """ Luo dictionary-tyyppisen ruudukon, jonka avulla seurataan labyrintissa 
+        kuljettuja reittejä.
+
+        Args:
+            n: Labyrintin leveys/korkeus.
+        Returns:
+            ruudukko, tyypiltään dictionary. Ruudukon arvot ilmaisevat, onko kyseiseen ruutuun
+            mahdollista kulkea. Ulkoseinät saavat arvon 3.
+        """
+
+        rivit = [i for i in range(0,n+2)] * (n+2)
+        sarakkeet = []
+        x = 0
+        while x <= n+1:
+            for i in range(0,n+2):
+                sarakkeet.append(x)
+            x += 1
+        arvot = [0] * (n+2) * (n+2)
+        ruudukko = {(x,y): val for x, y, val in zip(rivit,sarakkeet,arvot)}
+        for i in ruudukko:
+            if i[0] == 0 or i [1]==0 or i[0] == n+1 or i[1] == n+1:
+                ruudukko[i] = 3
+        return ruudukko
+
+    def etsi_ja_merkitse_umpikujat(self):
+        for x in range(1, self.n + 1):
+            for y in range(1, self.n + 1):
+                if self.laske_naapuriruudut((x,y), 1) == 1 and (x,y) != self.maali and (x,y) != self.aloitus:
+                    self.umpikujat.append((x,y))
+                    self.labyrintti.markCells.append((x,y))
+
+    def laske_naapuriruudut(self, ruutu, arvo):
+
+        """ Laskee joko seinien tai avonaisten polkujen määrän tietyssä ruudussa.
+
+        Args:
+            ruutu: metodille annettava tuple-muotoinen arvo. Ensimmäinen arvo on ruudun
+            x-koordinaatti ja toinen y-koordinaatti.
+            arvo: Joko 0 tai 1. Jos 0, niin seinä, jos 1, niin avoin polku.
+        Returns:
+            lkm: tyypiltään integer. Seinien/avointen polkujen lukumäärä.
+        """
+
+        x = ruutu[0]
+        y = ruutu[1]
+        lkm = 0
+        if self.labyrintti.maze_map[x,y]['N'] == arvo:
+            lkm += 1
+        if self.labyrintti.maze_map[x,y]['E'] == arvo:
+            lkm += 1
+        if self.labyrintti.maze_map[x,y]['W'] == arvo:
+            lkm += 1
+        if self.labyrintti.maze_map[x,y]['S'] == arvo:
+            lkm += 1
+        return lkm
+
+    def mahdolliset_liikkeet(self, ruutu):
+
+        """ Palauttaa listan, joka sisältää sellaiset ruudut, joihin on mahdollista
+        kulkea tietystä ruudusta. Lisäksi ilmaisee jokaisen mahdollisen ruudun kohdalla,
+        kuinka monta kertaa kyseisestä ruudusta on kuljettu.
+
+        Args:
+            ruutu: tuple-tyyppinen arvo, jonka ensimmäinen arvo on ruudun x-koordinaatti
+            ja toinen y-koordinaatti.
+        Returns:
+            mahdolliset: tyypiltään lista, joka sisältää mahdolliset liikkeet. Jokaisen alkion
+            nollas indeksi on kyseisen liikkeen x-koordinaatti, ensimmäinen y-koordinaatti ja
+            toinen indeksi kertoo, kuinka monta kertaa kyseisessä ruudussa on käyty.
+        """
+
+        x = ruutu[0]
+        y = ruutu[1]
+        mahdolliset = []
+        sallitut_liikkeet = [0,1]
+        if self.labyrintti.maze_map[x,y]['N'] == 1 and self.jaljet[x-1,y] in sallitut_liikkeet:
+            mahdolliset.append([x-1,y,self.jaljet[x-1,y]])
+        if self.labyrintti.maze_map[x,y]['E'] == 1 and self.jaljet[x,y+1] in sallitut_liikkeet:
+            mahdolliset.append([x,y+1,self.jaljet[x,y+1]])
+        if self.labyrintti.maze_map[x,y]['W'] == 1 and self.jaljet[x,y-1] in sallitut_liikkeet:
+            mahdolliset.append([x,y-1,self.jaljet[x,y-1]])
+        if self.labyrintti.maze_map[x,y]['S'] == 1 and self.jaljet[x+1,y] in sallitut_liikkeet:
+            mahdolliset.append([x+1,y,self.jaljet[x+1,y]])
+        return mahdolliset
+
+    def on_umpikuja(self, ruutu):
+
+        """ Palauttaa tiedon siitä, onko kyseinen ruutu umpikuja.
+
+        Args:
+            ruutu: tuple-tyyppinen arvo, joka kertoo sen ruudun, jonka halutaan selvittää olevan umpikuja.
+        Returns:
+            boolean arvon, true jos ruutu on umpikuja ja false jos ei ole umpikuja.
+        """
+
+        return len(self.mahdolliset_liikkeet(ruutu)) == 1
+
+    def dead_end_filling(self):
+
+        """ Löytää reitin labyrintista noudattaen dead-end filling algoritmia.
+
+        Returns:
+            etsinta: tyypiltään lista. Sisältää järjestyksessä ne ruudut, jotka
+            algoritmi käy läpi umpikujista jättäen jäljelle reitin, joka ratkaisee
+            labyrintin.
+            reitti: labyrintin ratkaisu reitti, tyypiltään lista.
+        """
+
+        self.etsi_ja_merkitse_umpikujat()
+        self.nykyinen_ruutu = self.umpikujat[0]
+
+        while True:
+
+            if self.nykyinen_ruutu == self.aloitus:
+                if len(self.umpikujat) == 0:
+                    break
+                else:
+                    self.nykyinen_ruutu = self.umpikujat[0]
+            if self.nykyinen_ruutu == self.maali:
+                self.etsinta.append(self.maali)
+            if self.on_umpikuja(self.nykyinen_ruutu):
+                self.etsinta.append(self.nykyinen_ruutu)
+                self.jaljet[self.nykyinen_ruutu[0], self.nykyinen_ruutu[1]] = 2
+                seuraava = self.mahdolliset_liikkeet(self.nykyinen_ruutu)
+                self.nykyinen_ruutu = (seuraava[0][0], seuraava[0][1])
+            else:
+                self.umpikujat.pop(0)
+                if len(self.umpikujat) == 0:
+                    break
+                self.nykyinen_ruutu = self.umpikujat[0]
+
+        self.nykyinen_ruutu = self.aloitus
+
+        while not self.maali_loytynyt:
+
+            self.reitti.append(self.nykyinen_ruutu)
+            self.jaljet[self.nykyinen_ruutu[0], self.nykyinen_ruutu[1]] = 2
+            if self.nykyinen_ruutu == self.maali:
+                self.maali_loytynyt = True
+                break
+            seuraava = self.mahdolliset_liikkeet(self.nykyinen_ruutu)
+            self.nykyinen_ruutu = (seuraava[0][0], seuraava[0][1])
+
+        return self.etsinta, self.reitti
+        
--- src/index.py
+++ src/index.py
@@ -1,20 +1,31 @@
-from importlib.resources import path
-from pyamaze import maze, agent
+from pyamaze import maze, agent, COLOR
 from tremaux import Tremaux
 from left_wall_follower import LeftWallFollower
+from dead_end_filler import DeadEndFiller
 
 def main(x):
     m = maze(x,x)
     m.CreateMaze()
+    #m.CreateMaze(loadMaze="data/maze-55.csv")
+
     #t = Tremaux(m, x)
     #tremaux_reitti = t.tremaux()
     #a=agent(m,filled=True,footprints=True)
     #m.tracePath({a:tremaux_reitti}, delay=100)
     #m.run()
-    f = LeftWallFollower(m, x)
-    wall_follower_reitti = f.wall_follower()
-    a=agent(m,filled=True, shape='arrow')
-    m.tracePath({a:wall_follower_reitti}, delay=200)
+
+    #f = LeftWallFollower(m, x)
+    #wall_follower_reitti = f.wall_follower()
+    #a=agent(m,filled=True, shape='arrow')
+    #m.tracePath({a:wall_follower_reitti}, delay=200)
+    #m.run()
+
+    d = DeadEndFiller(m, x)
+    dead_end_filler_etsinta, dead_end_filler_reitti = d.dead_end_filling()
+    a=agent(m,filled=True,footprints=True, color=COLOR.red)
+    b=agent(m,filled=True,footprints=True, color=COLOR.green)
+    m.tracePath({a:dead_end_filler_etsinta},showMarked=True, delay=200)
+    m.tracePath({b:dead_end_filler_reitti},showMarked=True, delay=200)
     m.run()
 
 if __name__ == '__main__':
